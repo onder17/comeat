d1f87da8298370c3d299108b82e32d5aa2148d1e
Görkem Önder <grkmonder17@gmail.com>
2025-11-01 14:25:24 +0300

    added the basics of the project

---
+++ src/main/java/com/onder17/comeat/ComeatApplication.java
@@ -0,0 +1,13 @@
+package com.onder17.comeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onder17/comeat/ComeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onder17.comeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
